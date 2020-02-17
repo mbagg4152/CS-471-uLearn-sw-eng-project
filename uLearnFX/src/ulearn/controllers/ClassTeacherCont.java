2756995847c71d3be8e283b9d4f8d64f4f504fd7
magg4152 <hort4152@kettering.edu>
2020-02-17 15:57:05 -0500

    made chat pages an xml file

--- uLearnFX/src/ulearn/controllers/ClassTeacherCont.java
+++ uLearnFX/src/ulearn/controllers/ClassTeacherCont.java
@@ -4,4 +4,8 @@ import javafx.scene.control.Button;
 
 public class ClassTeacherCont {
     public Button chatsvhomesubmit;
+    public Button chatsvhomesubmit1;
+    public Button chatsvhomesubmit2;
+    public Button chatsvhomesubmit3;
+    public Button chatsvhomesubmit4;
 }
