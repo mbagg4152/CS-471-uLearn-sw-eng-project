04a868a0241aa8f07b81fe841753a72c6e7e7798
magg4152 <hort4152@kettering.edu>
2020-02-26 14:19:58 -0500

    read in json as reg file

--- uLearnFX/src/ulearn/Main.java
+++ uLearnFX/src/ulearn/Main.java
@@ -9,6 +9,10 @@ import javafx.scene.text.Text;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main extends Application {
     // for login
     public Button loginBtn;
@@ -20,19 +24,37 @@ public class Main extends Application {
     // for login_popup
     public Text uNameInfo, pwdInfo, dispUName, dispPwd;
     public Button goHomeBtn;
-    public Tab chatTab = new Tab(), classTab = new Tab(), calTab = new Tab(), storageTab = new Tab(), settingsTab = new Tab();
+    public Tab chatTab = new Tab(), classTab = new Tab(), calTab = new Tab(), storageTab = new Tab(), settingsTab =
+            new Tab();
     public TabPane mainTabs = new TabPane();
     public WebView gCalWebView = new WebView();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    @Override public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("layout/login.fxml"));
         primaryStage.setTitle("uLearn");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setResizable(true);
         primaryStage.show();
         System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+        readJson();
 
+    }
+
+
+    void readJson() {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        try {
+            File myObj = new File("database.json");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                //System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
