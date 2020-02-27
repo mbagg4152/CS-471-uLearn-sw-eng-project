128bb8c3d3510a8e539089198ba7f3bbd6eb3d95
mag <hort4152@kettering.edu>
2020-02-26 21:48:34 -0500

    .

--- uLearnFX/src/ulearn/utils/JsonReader.java
+++ uLearnFX/src/ulearn/utils/JsonReader.java
@@ -15,37 +15,51 @@ public class JsonReader {
 
 	}
 
-	public void setJObj(JSONObject jo) {
-		this.jObj = jo;
-	}
-
-	public JSONObject getJObj() {
-		return jObj;
-	}
-
 	public void readJson() {
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-		JSONParser jsonParser = new JSONParser();
+		System.out.println("Working Directory = " + System.getProperty("user.dir")); // for debug purposes
+		JSONParser jsonParser = new JSONParser(); // reads json file
 		jObj = null;
 		try {
-			jObj = (JSONObject) jsonParser.parse(new FileReader("database.json"));
+			jObj = (JSONObject) jsonParser.parse(new FileReader("database.json")); // try to parse json
 		} catch (IOException | ParseException e) { e.printStackTrace(); }
 		assert jObj != null;
 		setJObj(jObj);
-		String someId = getSingleItem("classes", "classId", "c125", "name");
+		String someId = getSingleItem("classes", "classId", "c125", "name"); // get some other value using a known value
 		System.out.println("got value " + someId + " from using id c125");
+		printContents("users", "name", "classList"); // print users & their classes
 
 	}
 
-	// pass in value that is known to get array index and using it to get another item
-	// ex: given an id get a class name
+	// give object type and list name to print out list
+	public void printContents(String objType, String key, String listName) {
+		JSONArray arr = (JSONArray) jObj.get(objType);
+		for (Object obj : arr) {
+			JSONObject tmp = (JSONObject) obj;
+			System.out.print("Label: " + ((String) tmp.get(key)) + " --> ");
+			if (objType.equals("users")) {
+				String type = (String) tmp.get("userType");
+				if (!type.equals("admin")) {
+					System.out.print(" contents: ");
+					JSONArray jArr = (JSONArray) tmp.get(listName);
+					for (Object t : jArr) { System.out.print(" " + t); }
+				} else { System.out.print("no sub list"); }
+			} else {
+				System.out.println(" contents: ");
+				JSONArray jArr = (JSONArray) tmp.get(listName);
+				for (Object t : jArr) { System.out.print(" " + t); }
+			}
+
+			System.out.println();
+		}
+	}
+
+	// pass in value that is known to get array index and using it to get another item. ex: given an id get a class name
 	String getSingleItem(String itemType, String knownKey, String knownValue, String lookFor) {
 		JSONArray userArr = (JSONArray) jObj.get(itemType);
 		int index = 0, counter = 0;
 		if (userArr != null) {
 			for (Object ob : userArr) {
-				JSONObject tmp = (JSONObject) ob;
-				String tmpVal = (String) tmp.get(knownKey);
+				String tmpVal = (String) ((JSONObject) ob).get(knownKey);
 				if (tmpVal.equals(knownValue)) {
 					index = counter;
 					break;
@@ -58,34 +72,16 @@ public class JsonReader {
 		return "error";
 	}
 
+	// to be used to get lists from something, like get list of classes from user or get list of students in classes
 	public void getItemList() {}
 
-	public void printContents(JSONObject jObj, String objectLabel, String arrKey, String listName, String outerLabel, String innerLabel) {
-		JSONArray userArr = (JSONArray) jObj.get(objectLabel);
-		for (Object obj : userArr) {
-			JSONObject tmp = (JSONObject) obj;
-			String name = (String) tmp.get(arrKey);
+	// either to confirm something is in a list or used to get other contents of a list, TBD
+	public void getSingleItemFromList() {
 
-			System.out.print(outerLabel + " " + name + ". ");
-			if (objectLabel.equals("users")) {
-				String type = (String) tmp.get("userType");
-
-				if (!type.equals("admin")) {
-					JSONArray jArr = (JSONArray) tmp.get(listName);
-					for (Object t : jArr) {
-						System.out.print(" " + innerLabel + " " + t);
-					}
-
-				}
-			} else {
-				JSONArray jArr = (JSONArray) tmp.get(listName);
-				for (Object t : jArr) {
-					System.out.print(" " + innerLabel + " " + t);
-				}
-
-			}
-			System.out.println("");
-		}
 	}
+
+	public void setJObj(JSONObject jo) { this.jObj = jo; }
+
+	public JSONObject getJObj() { return jObj; }
 
 }
