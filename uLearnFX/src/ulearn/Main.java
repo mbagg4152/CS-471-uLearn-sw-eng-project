23e0f306dc1d7a6c3263205a3932be9a3550fccd
mag <hort4152@kettering.edu>
2020-02-28 18:47:18 -0500

    .

--- uLearnFX/src/ulearn/Main.java
+++ uLearnFX/src/ulearn/Main.java
@@ -11,36 +11,35 @@ import javafx.stage.Stage;
 import ulearn.utils.JsonReader;
 
 public class Main extends Application {
-    // for login
-    public Button loginBtn;
-    public Label unameLabel, pwdLabel;
-    public TextField unameBox;
-    public PasswordField pwdBox;
+	// for login
+	public Button loginBtn;
+	public Label unameLabel, pwdLabel;
+	public TextField unameBox;
+	public PasswordField pwdBox;
 
+	// for login_popup
+	public Text uNameInfo, pwdInfo, dispUName, dispPwd;
+	public Button goHomeBtn;
+	public Tab chatTab = new Tab(), classTab = new Tab(), calTab = new Tab(), storageTab = new Tab(), settingsTab =
+			new Tab();
+	public TabPane mainTabs = new TabPane();
+	public WebView gCalWebView = new WebView();
 
-    // for login_popup
-    public Text uNameInfo, pwdInfo, dispUName, dispPwd;
-    public Button goHomeBtn;
-    public Tab chatTab = new Tab(), classTab = new Tab(), calTab = new Tab(), storageTab = new Tab(), settingsTab =
-            new Tab();
-    public TabPane mainTabs = new TabPane();
-    public WebView gCalWebView = new WebView();
+	@Override public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("layout/login.fxml"));
+		primaryStage.setTitle("uLearn");
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.setResizable(true);
+		primaryStage.show();
+		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+		readJson();
 
-    @Override public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("layout/login.fxml"));
-        primaryStage.setTitle("uLearn");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.setResizable(true);
-        primaryStage.show();
-        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
-        readJson();
+	}
 
-    }
+	void readJson() {
+		JsonReader jReader = new JsonReader();
+		jReader.readJson();
+	}
 
-    void readJson() {
-        JsonReader jReader = new JsonReader();
-        jReader.readJson();
-    }
-
-    public static void main(String[] args) { launch(args); }
+	public static void main(String[] args) { launch(args); }
 }
--- uLearnFX/src/ulearn/controllers/InitCont.java
+++ uLearnFX/src/ulearn/controllers/InitCont.java
@@ -18,45 +18,49 @@ import java.io.IOException;
 import static javafx.fxml.FXMLLoader.load;
 
 public class InitCont {
-    // for login
-    public Label title = new Label();
-    public Label unameLabel = new Label(), pwdLabel = new Label();
-    public TextField unameBox = new TextField();
-    public PasswordField pwdBox = new PasswordField();
-    public Button loginBtn = new Button();
+	// for login
+	public Label title = new Label();
+	public Label unameLabel = new Label(), pwdLabel = new Label();
+	public TextField unameBox = new TextField();
+	public PasswordField pwdBox = new PasswordField();
+	public Button loginBtn = new Button();
 
-    // for login_popup
-    public Label uNameInfo = new Label(), pwdInfo = new Label(), dispUName = new Label(), dispPwd = new Label();
-    public Button goHomeBtn = new Button();
+	// for login_popup
+	public Label uNameInfo = new Label(), pwdInfo = new Label(), dispUName = new Label(), dispPwd = new Label();
+	public Button goHomeBtn = new Button();
 
-    @FXML private void loginBtnAction(ActionEvent event) throws IOException {
-        event.consume();
-        if (verifyUser(unameBox.getText(),pwdBox.getText())){
-            String path = "../layout/main_screen.fxml";
-            setupOnClick(path, loginBtn, 600, 350, true);
-        }
-        else {
-            JOptionPane.showMessageDialog(null,"Incorrect Username or Password");
-        }
-    }
+	@FXML private void loginBtnAction(ActionEvent event) throws IOException {
+		event.consume();
+		if (verifyUser(unameBox.getText(), pwdBox.getText())) {
+			String path = "../layout/main_screen.fxml";
+			setupOnClick(path, loginBtn, 600, 350, true);
+		} else {
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+				e.printStackTrace();
+			}
+			JOptionPane.showMessageDialog(null, "Incorrect Username or Password");
+		}
+	}
 
-    private Boolean verifyUser(String username,String password) {
-        // Compare pass against username pass
-        JsonReader database = new JsonReader();
-        database.readJson();
-        return password.equals(database.getSingleItem("users", "userId", username, "password"));
-    }
+	private Boolean verifyUser(String username, String password) {
+		// Compare pass against username pass
+		JsonReader database = new JsonReader();
+		database.readJson();
+		return password.equals(database.getSingleItem("users", "userId", username, "password"));
+	}
 
-    void setupOnClick(String layout, Button btn, int width, int height, boolean resize) throws IOException {
-        Parent par = FXMLLoader.load(getClass().getResource(layout));
-        Stage stage = (Stage) btn.getScene().getWindow();
-        Scene scene = new Scene(par, width, height);
-        scene.setFill(null);
-        stage.setScene(scene);
-        stage.setResizable(resize);
-        stage.show();
-        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
-        stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
-        stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 4);
-    }
+	void setupOnClick(String layout, Button btn, int width, int height, boolean resize) throws IOException {
+		Parent par = FXMLLoader.load(getClass().getResource(layout));
+		Stage stage = (Stage) btn.getScene().getWindow();
+		Scene scene = new Scene(par, width, height);
+		scene.setFill(null);
+		stage.setScene(scene);
+		stage.setResizable(resize);
+		stage.show();
+		Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+		stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
+		stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 4);
+	}
 }
